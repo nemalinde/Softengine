cd9b6fe282585c8c1badf95e98cb71212996bbab
lindelani <lindelani@takealot.com>
2018-04-14 13:25:03 +0200

    priventif web app

---
+++ priventif/inventory_service/inventory_service/views.py
@@ -0,0 +1,11 @@
+from pyramid.view import view_config
+
+
+@view_config(route_name='home', renderer='templates/home_template.pt')
+def my_view(request):
+    return {'project': 'inventory_service'}
+
+
+@view_config(route_name='index', renderer='static/html/index.pt')
+def index(request):
+    return {'project': 'inventory_service'}
