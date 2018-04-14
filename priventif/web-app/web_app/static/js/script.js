4382a651fddf5191073f6d7c2e9943b6642bf034
lindelani <lindelani@takealot.com>
2018-04-14 15:05:42 +0200

    refactor name

---
+++ priventif/web-app/web_app/static/js/script.js
@@ -0,0 +1,69 @@
+jQuery(function ($) {
+
+    'use strict';
+
+
+    
+    $(window).resize(function() {
+        
+
+    });
+    
+    $(document).ready(function(){
+       
+        // $('.loading').fadeOut(500);
+
+        // var x = $("#sidebar-list").offset();
+
+        // $(".search-btn").click(function() {
+        //     $(".dropdown-form").toggle("");
+        // });
+
+        // $(".message-top i").click(function() {
+        //     $(".message-top form input").toggle("");
+        // });
+        
+        // if($(window).width() < 768) {
+        //     $(".sub-items").offset({top:x.top,});
+        //     $("#sidebar-list>li>a").click(function() {
+        //         $(".sub-items").toggle("");
+        //     });
+        // }
+
+        // $(".how-it-works-section").css("visibility", "hidden");
+        // $(".how-it-works-section").waypoint(function () {
+        //     $(".how-it-works-section").addClass("fadeInUp");
+        //     $(".how-it-works-section").css("visibility", "visible");
+        // }, {
+        //     offset: "60%"
+        // });
+
+        // $(".about-us-section>div").css("visibility", "hidden");
+        // $(".about-us-section>div").waypoint(function () {
+        //     $(".about-us-section>div").addClass("fadeInLeft");
+        //     $(".about-us-section>div").css("visibility", "visible");
+        // }, {
+        //     offset: "60%"
+        // });
+
+        // $(".contact-us-section").css("visibility", "hidden");
+        // $(".contact-us-section").waypoint(function () {
+        //     $(".contact-us-section").addClass("fadeInRight");
+        //     $(".contact-us-section").css("visibility", "visible");
+        // }, {
+        //     offset: "60%"
+        // });
+
+        // $("footer").css("visibility", "hidden");
+        // $("footer").waypoint(function () {
+        //     $("footer").addClass("fadeInDown");
+        //     $("footer").css("visibility", "visible");
+        // }, {
+        //     offset: "80%"
+        // });
+
+
+    });
+
+}); // JQuery end
+
